339699b414c68cfe92c51f2892303903301b5374
Tercio Misael <terciomisael@gmail.com>
2021-09-09 12:32:03 -0300

    Basic Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable />
+
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
