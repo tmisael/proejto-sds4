c761e73b70f86eb7595337ed98a92fd210af85bf
Tercio Misael <terciomisael@gmail.com>
2021-09-12 21:08:20 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,12 @@
-import NavBar from "components/NavBar";
-import Footer from "components/Footer";
-import DataTable from "components/DataTable";
-import BarChart from "components/BarChart";
-import DonutChart from "components/DonutChart";
+
+import Routes from "Routes";
 
 
 function App() {
   return (
-
-    <>
-      <NavBar />
-
-      <div className="container">
-        <h1 className="text-primary py-3">Painel de Vendas</h1>
-
-        <div className="row px-3">
-
-          <div className="col-sm-6">
-
-            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
-            <BarChart />
-
-          </div>
-          <div className="col-sm-6">
-
-          <h5 className="text-center text-secondary">Todas as vendas</h5>
-            <DonutChart />
-
-          </div>
-          
-
-        </div>
-        <h2 className="text-primary py-3">Todas as Vendas</h2>
-        <DataTable />
-      
-
-        <Footer />
-      </div>
-    </>
+    <Routes />
+     
+   
   );
 }
 
---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,26 @@
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Switch, Route  } from 'react-router-dom';
+
+
+
+function Routes(){
+    return (
+
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact>
+                    <Home />
+                </Route>
+            </Switch>
+            <Switch>
+                <Route path='/dashboard'>
+                    <Dashboard />
+                </Route>
+            </Switch>
+
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
