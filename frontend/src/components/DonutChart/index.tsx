01737327f6c7b38f4942a84d1718fddad369f81a
Tercio Misael <terciomisael@gmail.com>
2021-09-12 23:29:00 -0300

    Barchart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -11,42 +11,42 @@ type ChartData = {
 }
 
 function DonutChart() {
-   
-    const [chartData, setChartData] = useState<ChartData>( {labels: [], series: []} )
 
-    useEffect(()=>{
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
+
+    useEffect(() => {
 
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response=>{
+            .then(response => {
 
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName)
-            const mySeries = data.map(x => x.sum)
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName)
+                const mySeries = data.map(x => x.sum)
 
-            setChartData ({ labels: myLabels, series: mySeries})
-            console.log(chartData);
+                setChartData({ labels: myLabels, series: mySeries })
+                console.log(chartData);
 
-        })
-    },[] )
+            })
+    }, [])
 
-  
+
 
     const options = {
         legend: {
             show: true
         }
     }
-    
-    
-    
-    return (
-       <Chart 
-        options = {{ ...options, labels: chartData.labels}}
-        series = {chartData.series}
-        type = "donut"
-        height = "240"
 
-       />
+
+
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+
+        />
     );
 }
 
